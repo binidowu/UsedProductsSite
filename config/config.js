3deeaa14bdd9f5312cfe79436f747727ee080338
Adebayo Ayomide <adebayormide@gmail.com>
2023-11-07 19:08:14 -0500

    Correcting DB name

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // Exporting the MongoDB Atlas connection string
 module.exports = {
     "SECRETKEY": "Your_Secret_goes_here",
-    "ATLASDB": "mongodb+srv://binidowu:yxydFKxFEJVbpIdx@cluster229.dnxi8sd.mongodb.net/DressStore?retryWrites=true&w=majority",
+    "ATLASDB": "mongodb+srv://binidowu:yxydFKxFEJVbpIdx@cluster229.dnxi8sd.mongodb.net/Web9JA?retryWrites=true&w=majority",
 };
 
