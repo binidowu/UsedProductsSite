27c5a8718a87a96ab9f2b16677813ce6da586c44
Yusuf O Adeleke <66679328+y-adeleke@users.noreply.github.com>
2023-11-20 21:10:21 -0500

    WEB9JA-35 I reviewed Ayo's code, made a lil changes and added the favorites functionality

--- web9ja_backend/routes/ads.js
+++ web9ja_backend/routes/ads.js
@@ -5,11 +5,25 @@ let router = express.Router();
 
 let adsController = require("../controllers/ads");
 
-router.post('/', requireSignin, adsController.createAd); // Post a new ad
-router.put('/:adID', requireSignin, adsController.isOwner, adsController.editAd); // Edit a specific ad
-router.get('/', adsController.viewAds); // View all ads
-router.put('/:adID/disable', requireSignin, adsController.isOwner, adsController.disableAd);// Toggle staus of a specific ad
-router.post('/:adID/questions', adsController.askQuestion); //asking a question on an ad.
-router.post('/:adID/questions/:questionID/answer', requireSignin, adsController.isOwner, adsController.answerQuestion); // answering a question on an ad
+// Post a new ad
+router.post("/", requireSignin, adsController.createAd);
+
+// Edit a specific ad
+router.put("/:adID", requireSignin, adsController.isOwner, adsController.editAd);
+
+// View all ads
+router.get("/", adsController.viewAds);
+
+// Toggle staus of a specific ad
+router.put("/disable/:adID", requireSignin, adsController.isOwner, adsController.disableAd);
+
+//asking a question on an ad.
+router.post("/questions/:adID", adsController.askQuestion);
+
+// answering a question on an ad
+router.post("/questions/:adID/answer/:questionID", requireSignin, adsController.isOwner, adsController.answerQuestion);
+
+//add or remove ad from favorites
+router.put("/favorites/:userID/:adID", requireSignin, adsController.toggleFavoriteAd);
 
 module.exports = router;
